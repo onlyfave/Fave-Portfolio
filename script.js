166d099244d61c8985c2044c78bd682a57dc63b4
Fave <onyenekeflavour@gmail.com>
2024-12-16 19:36:02 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
+
+navToggle.addEventListener("click", () => {
+  return console.log(navToggle);
+});
