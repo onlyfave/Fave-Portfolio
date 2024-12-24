0e8e6ace0e0dd0c7af51284bcf396099bc18acb7
Olatoyan <golatoyan@gmail.com>
2024-12-24 11:12:09 +0100

    Remove unused CSS and HTML files

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const navToggle = document.getElementById("navToggle");
-const navLinks = document.getElementById("navLinks");
-
-navToggle.addEventListener("click", () => {
-  return console.log(navToggle);
-});
